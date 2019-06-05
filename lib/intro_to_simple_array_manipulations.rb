1764fb40d2b48012d5fb3e2650fcd0ccbf079892
magical-flow-3186 <rregalado17@gmail.com>
2019-06-05 00:39:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,6 +11,7 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
 end
 
 def using_pop(great_hits_of_the_nineties)
+<<<<<<< HEAD
   great_hits_of_the_nineties.pop
 end
 
@@ -70,3 +71,8 @@ end
 
 
 
+=======
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  great_hits_of_the_nineties.pop
+end
+>>>>>>> c0b01255fc6c44dc7a0550965ea4a982285a0d7d
